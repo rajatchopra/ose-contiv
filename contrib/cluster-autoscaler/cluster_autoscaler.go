670d2987849025b85cf71f4108d7603f9e96d2ba
Rajat Chopra <rchopra@redhat.com>
2016-06-17 11:12:02 -0700

    kube contrib import

---
+++ contrib/cluster-autoscaler/cluster_autoscaler.go
@@ -0,0 +1,183 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"net/url"
+	"time"
+
+	"k8s.io/contrib/cluster-autoscaler/config"
+	"k8s.io/contrib/cluster-autoscaler/estimator"
+	"k8s.io/contrib/cluster-autoscaler/simulator"
+	"k8s.io/contrib/cluster-autoscaler/utils/gce"
+	kube_client "k8s.io/kubernetes/pkg/client/unversioned"
+
+	"github.com/golang/glog"
+)
+
+var (
+	migConfigFlag config.MigConfigFlag
+	kubernetes    = flag.String("kubernetes", "", "Kuberentes master location. Leave blank for default")
+)
+
+// ExpansionOption describes an option to expand the cluster.
+type ExpansionOption struct {
+	migConfig *config.MigConfig
+	estimator *estimator.BasicNodeEstimator
+}
+
+func main() {
+	flag.Var(&migConfigFlag, "nodes", "sets min,max size and url of a MIG to be controlled by Cluster Autoscaler. "+
+		"Can be used multiple times. Format: <min>:<max>:<migurl>")
+	flag.Parse()
+
+	url, err := url.Parse(*kubernetes)
+	if err != nil {
+		glog.Fatalf("Failed to parse Kuberentes url: %v", err)
+	}
+	kubeConfig, err := config.GetKubeClientConfig(url)
+	if err != nil {
+		glog.Fatalf("Failed to build Kuberentes client configuration: %v", err)
+	}
+
+	kubeClient := kube_client.NewOrDie(kubeConfig)
+	unscheduledPodLister := NewUnscheduledPodLister(kubeClient)
+	nodeLister := NewNodeLister(kubeClient)
+
+	migConfigs := make([]*config.MigConfig, 0, len(migConfigFlag))
+	for i := range migConfigFlag {
+		migConfigs = append(migConfigs, &migConfigFlag[i])
+	}
+
+	gceManager, err := gce.CreateGceManager(migConfigs)
+	if err != nil {
+		glog.Fatalf("Failed to create GCE Manager %v", err)
+	}
+
+	predicateChecker := simulator.NewPredicateChecker()
+
+	for {
+		select {
+		case <-time.After(time.Minute):
+			{
+				pods, err := unscheduledPodLister.List()
+				if err != nil {
+					glog.Errorf("Failed to list pods: %v", err)
+					continue
+				}
+				if len(pods) == 0 {
+					glog.V(1).Info("No unscheduled pods")
+					continue
+				}
+
+				for _, pod := range pods {
+					glog.V(1).Infof("Pod %s/%s is not scheduled", pod.Namespace, pod.Name)
+				}
+
+				nodes, err := nodeLister.List()
+				if err != nil {
+					glog.Errorf("Failed to list nodes: %v", err)
+					continue
+				}
+				if len(nodes) == 0 {
+					glog.Errorf("No nodes in the cluster")
+					continue
+				}
+
+				if err := CheckMigsAndNodes(nodes, gceManager); err != nil {
+					glog.Warningf("Cluster is not ready for autoscaling: %v", err)
+					continue
+				}
+
+				// Checks if scheduler tried to schedule the pods after thew newest node was added.
+				newestNode := GetNewestNode(nodes)
+				if newestNode == nil {
+					glog.Errorf("No newest node")
+					continue
+				}
+				oldestSchedulingTrial := GetOldestFailedSchedulingTrail(pods)
+				if oldestSchedulingTrial == nil {
+					glog.Errorf("No oldest unschedueled trial: %v", err)
+					continue
+				}
+
+				// TODO: Find better way to check if all pods were checked after the newest node
+				// was added.
+				if newestNode.CreationTimestamp.After(oldestSchedulingTrial.Add(-1 * time.Minute)) {
+					// Lets give scheduler another chance.
+					glog.V(1).Infof("One of the pods have not been tried after adding %s", newestNode.Name)
+					continue
+				}
+
+				expansionOptions := make([]ExpansionOption, 0)
+				nodeInfos, sampleNodes, err := GetNodeInfosForMigs(nodes, gceManager, kubeClient)
+				if err != nil {
+					glog.Errorf("Failed to build node infors for migs: %v", err)
+					continue
+				}
+
+				for _, migConfig := range migConfigs {
+					option := ExpansionOption{
+						migConfig: migConfig,
+						estimator: estimator.NewBasicNodeEstimator(),
+					}
+					migHelpsSomePods := false
+
+					nodeInfo, found := nodeInfos[migConfig.Url()]
+					if !found {
+						glog.Errorf("No node info for: %s", migConfig.Url())
+						continue
+					}
+
+					node, found := sampleNodes[migConfig.Url()]
+					if !found {
+						glog.Errorf("No sample node for: %s", migConfig.Url())
+						continue
+					}
+
+					for _, pod := range pods {
+						err = predicateChecker.CheckPredicates(pod, node, nodeInfo)
+						if err == nil {
+							migHelpsSomePods = true
+							option.estimator.Add(pod)
+						} else {
+							glog.V(2).Infof("Scale-up predicate failed: %v", err)
+						}
+					}
+					if migHelpsSomePods {
+						expansionOptions = append(expansionOptions, option)
+					}
+				}
+
+				// Pick some expansion option.
+				bestOption := BestExpansionOption(expansionOptions)
+				if bestOption != nil {
+					estimate := bestOption.estimator.Estimate(bestOption.migConfig.Node())
+					currentSize, err := gceManager.GetMigSize(bestOption.migConfig)
+					if err != nil {
+						glog.Errorf("Failed to get MIG size: %v", err)
+						continue
+					}
+					if err := gceManager.SetMigSize(bestOption.migConfig, currentSize+int64(estimate)); err == nil {
+						glog.Errorf("Failed to set: %v", err)
+					}
+				}
+			}
+		}
+	}
+}
---
+++ contrib/cluster-autoscaler/utils.go
@@ -0,0 +1,181 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"k8s.io/contrib/cluster-autoscaler/config"
+	"k8s.io/contrib/cluster-autoscaler/simulator"
+	"k8s.io/contrib/cluster-autoscaler/utils/gce"
+
+	kube_api "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/client/cache"
+	kube_client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/plugin/pkg/scheduler/schedulercache"
+)
+
+// UnscheduledPodLister list unscheduled pods
+type UnscheduledPodLister struct {
+	podLister *cache.StoreToPodLister
+}
+
+// List returns all unscheduled pods.
+func (unscheduledPodLister *UnscheduledPodLister) List() ([]*kube_api.Pod, error) {
+	//TODO: Extra filter based on pod condition.
+	return unscheduledPodLister.podLister.List(labels.Everything())
+}
+
+// NewUnscheduledPodLister returns a lister providing pods that failed to be scheduled.
+func NewUnscheduledPodLister(kubeClient *kube_client.Client) *UnscheduledPodLister {
+	// watch unscheduled pods
+	selector := fields.ParseSelectorOrDie("spec.nodeName==" + "" + ",status.phase!=" +
+		string(kube_api.PodSucceeded) + ",status.phase!=" + string(kube_api.PodFailed))
+	podListWatch := cache.NewListWatchFromClient(kubeClient, "pods", kube_api.NamespaceAll, selector)
+	podLister := &cache.StoreToPodLister{Store: cache.NewStore(cache.MetaNamespaceKeyFunc)}
+	podReflector := cache.NewReflector(podListWatch, &kube_api.Pod{}, podLister.Store, time.Hour)
+	podReflector.Run()
+
+	return &UnscheduledPodLister{
+		podLister: podLister,
+	}
+}
+
+// ReadyNodeLister lists ready nodes.
+type ReadyNodeLister struct {
+	nodeLister *cache.StoreToNodeLister
+}
+
+// List returns ready nodes.
+func (readyNodeLister *ReadyNodeLister) List() ([]*kube_api.Node, error) {
+	nodes, err := readyNodeLister.nodeLister.List()
+	if err != nil {
+		return []*kube_api.Node{}, err
+	}
+	readyNodes := make([]*kube_api.Node, 0, len(nodes.Items))
+	for i, node := range nodes.Items {
+		for _, condition := range node.Status.Conditions {
+			if condition.Type == kube_api.NodeReady && condition.Status == kube_api.ConditionTrue {
+				readyNodes = append(readyNodes, &nodes.Items[i])
+				break
+			}
+		}
+	}
+	return readyNodes, nil
+}
+
+// NewNodeLister builds a node lister.
+func NewNodeLister(kubeClient *kube_client.Client) *ReadyNodeLister {
+	listWatcher := cache.NewListWatchFromClient(kubeClient, "nodes", kube_api.NamespaceAll, fields.Everything())
+	nodeLister := &cache.StoreToNodeLister{Store: cache.NewStore(cache.MetaNamespaceKeyFunc)}
+	reflector := cache.NewReflector(listWatcher, &kube_api.Node{}, nodeLister.Store, time.Hour)
+	reflector.Run()
+	return &ReadyNodeLister{
+		nodeLister: nodeLister,
+	}
+}
+
+// GetNewestNode returns the newest node from the given list.
+func GetNewestNode(nodes []*kube_api.Node) *kube_api.Node {
+	var result *kube_api.Node
+	for _, node := range nodes {
+		if result == nil || node.CreationTimestamp.After(result.CreationTimestamp.Time) {
+			result = node
+		}
+	}
+	return result
+}
+
+// GetOldestFailedSchedulingTrail returns the oldest time when a pod from the given list failed to
+// be scheduled.
+func GetOldestFailedSchedulingTrail(pods []*kube_api.Pod) *time.Time {
+	// Dummy implementation.
+	//TODO: Implement once pod condition is there.
+	now := time.Now()
+	return &now
+}
+
+// CheckMigsAndNodes checks if all migs have all required nodes.
+func CheckMigsAndNodes(nodes []*kube_api.Node, gceManager *gce.GceManager) error {
+	migCount := make(map[string]int)
+	migs := make(map[string]*config.MigConfig)
+	for _, node := range nodes {
+		instanceConfig, err := config.InstanceConfigFromProviderId(node.Spec.ProviderID)
+		if err != nil {
+			return err
+		}
+
+		migConfig, err := gceManager.GetMigForInstance(instanceConfig)
+		if err != nil {
+			return err
+		}
+		url := migConfig.Url()
+		count, _ := migCount[url]
+		migCount[url] = count + 1
+		migs[url] = migConfig
+	}
+	for url, mig := range migs {
+		size, err := gceManager.GetMigSize(mig)
+		if err != nil {
+			return err
+		}
+		count := migCount[url]
+		if size != int64(count) {
+			return fmt.Errorf("wrong number of nodes for mig: %s expected: %d actual: %d", url, size, count)
+		}
+	}
+	return nil
+}
+
+// GetNodeInfosForMigs finds NodeInfos for all migs used to manage the given nodes. It also returns a mig to sample node mapping.
+func GetNodeInfosForMigs(nodes []*kube_api.Node, gceManager *gce.GceManager, kubeClient *kube_client.Client) (map[string]*schedulercache.NodeInfo,
+	map[string]*kube_api.Node, error) {
+	sampleNodes := make(map[string]*kube_api.Node)
+	for _, node := range nodes {
+		instanceConfig, err := config.InstanceConfigFromProviderId(node.Spec.ProviderID)
+		if err != nil {
+			return map[string]*schedulercache.NodeInfo{}, map[string]*kube_api.Node{}, err
+		}
+
+		migConfig, err := gceManager.GetMigForInstance(instanceConfig)
+		if err != nil {
+			return map[string]*schedulercache.NodeInfo{}, map[string]*kube_api.Node{}, err
+		}
+		url := migConfig.Url()
+		sampleNodes[url] = node
+	}
+	result := make(map[string]*schedulercache.NodeInfo)
+	for url, node := range sampleNodes {
+		nodeInfo, err := simulator.BuildNodeInfoForNode(node.Name, kubeClient)
+		if err != nil {
+			return map[string]*schedulercache.NodeInfo{}, map[string]*kube_api.Node{}, err
+		}
+		result[url] = nodeInfo
+	}
+	return result, sampleNodes, nil
+}
+
+// BestExpansionOption picks the best cluster expansion option.
+func BestExpansionOption(expansionOptions []ExpansionOption) *ExpansionOption {
+	if len(expansionOptions) > 0 {
+		return &expansionOptions[0]
+	}
+	return nil
+}
