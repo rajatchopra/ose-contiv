1fec78e6f4e5bfa66a73c85b88511adb03168d0e
Rajat Chopra <rchopra@redhat.com>
2016-07-11 12:29:30 -0700

    add sed commands to scp script

--- scp.sh
+++ scp.sh
@@ -1,1 +1,3 @@
 for i in `echo "$1 $2 $3 $4"`; do scp -i ~/.ssh/libra.pem $5 centos@${i}:/tmp/id; ssh -i ~/.ssh/libra.pem centos@${i} 'cat /tmp/id >> ~/.ssh/authorized_keys'; done
+for i in `echo "$2 $3 $4"`; do ssh centos@${i} "sudo sed -i 's/redhat\/openshift-ovs-subnet/cni/g' /etc/origin/node/node-config.yaml"; done
+for i in `echo "$1"`; do ssh centos@${i} "sudo sed -i 's/redhat\/openshift-ovs-subnet/cni/g' /etc/origin/master/master-config.yaml"; done
